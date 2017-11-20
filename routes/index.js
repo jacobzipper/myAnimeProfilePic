9f149b8a9af136f524122e6475b90eaff50647af
User <zipper@jacobzipper.com>
2017-11-19 22:29:58 -0500

    basic UI

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user/:name', function(req,res,next) {
+  res.json({'url':'https://s3.amazonaws.com/myanimeprofilepic/'+req.params.name+'.png'});
+});
+
 module.exports = router;

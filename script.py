08c308919cd963ac07a364b481a155e0f3e3dc8c
User <zipper@jacobzipper.com>
2017-11-20 21:21:30 -0500

    script works!

--- script.py
+++ script.py
@@ -9,6 +9,8 @@ import os
 import hashlib
 import math
 import imp
+import random
+
 utils = imp.load_source("utils","/usr/lib/python2.7/site-packages/google/modules/utils.py")
 calculator = imp.load_source("calculator","/usr/lib/python2.7/site-packages/google/modules/calculator.py")
 currency = imp.load_source("currency","/usr/lib/python2.7/site-packages/google/modules/currency.py")
@@ -24,17 +26,26 @@ user = sys.argv[1]
 response = requests.get("https://myanimelist.net/malappinfo.php?status=all&type=anime&u="+user)
 anime = json.loads(json.dumps(xmltodict.parse(response.content)))["myanimelist"]["anime"]
 anime = [a["series_title"] for a in anime]
-md5user = hashlib.md5(user.encode())
-picsDl = math.ceil(100/float(len(anime)))
+if len(anime) == 0:
+  print "add anime to your profile please"
+  sys.exit(0)
+trimmedAnime = []
+for i in xrange(min(len(anime),5)):
+  trimmedAnime.append(anime.pop(random.randint(0,len(anime)-1)))
+md5user = hashlib.md5(user.encode()).hexdigest()
+os.system("mkdir " + md5user)
+picsDl = math.ceil(100/float(len(trimmedAnime)))
 print "downloading "+str(picsDl)+" per anime"
 options = images.ImageOptions()
 options.size_category = images.SizeCategory.SMALL
-for anim in anime:
+for anim in trimmedAnime:
   results = google.search_images(anim, options)
-  print results
+  images.fast_download(results[:int(picsDl)], path = md5user, threads=12)
+os.system("python mosaic.py faces/"+hashlib.md5((user+"face").encode()).hexdigest()+".png "+md5user+"/ "+user)
 keys = json.loads(open("config.json").read())
 conn = S3Connection(keys['access'], keys['secret'])
 mal = Key(conn.get_bucket('myanimeprofilepic'))
 mal.key = user+".png"
 mal.set_contents_from_filename(mal.key)
+os.system("rm -rf faces/"+hashlib.md5((user+"face").encode()).hexdigest()+".png "+md5user+" "+user+".png")
 print "done"
